57070d80b313af07fac3aa1b985e42827b6e2213
VitalyW-Git <>
2023-06-14 00:41:54 +0300

    add file docker compose, settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
+import { configModule } from '@src/config/configure.root';
+import { ProductsModule } from '@src/products/products.module';
+import { UsersModule } from '@src/users/users.module';
+import { CatalogOrderModule } from '@src/catalog-order/catalog-order.module';
+import { CatalogOrderItemsModule } from '@src/catalog-order-items/catalog-order-items.module';
+import { sequelizeConnectDb } from '@src/config/sequelize-connect.db';
 import { Module } from '@nestjs/common';
-import { configModule } from './config/configure.root';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ProductsModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
 import { ConfigService } from '@nestjs/config';
-import { CatalogOrderModule } from './catalog-order/catalog-order.module';
-import { CatalogOrderItemsModule } from './catalog-order-items/catalog-order-items.module';
-import { sequelizeConnectDb } from './config/sequelize-connect.db';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -14,6 +16,9 @@ import { sequelizeConnectDb } from './config/sequelize-connect.db';
     SequelizeModule.forRootAsync({
       useFactory: sequelizeConnectDb,
       inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
     }),
     ProductsModule,
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { AppModule } from '@src/app.module';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
