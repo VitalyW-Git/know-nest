e2db4a7cb680320efd3672b169809cf448fba4df
VitalyW-Git <>
2023-07-04 22:33:01 +0300

    redis connect

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,41 @@
 import { AppModule } from '@src/app.module';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as session from 'express-session';
+import * as connectRedis from 'connect-redis';
+import { createClient } from 'redis';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  // Подключение Redis в качестве базы данных с помощью сеансов
+  const RedisStore = connectRedis(session);
+  const redisClient: any = createClient({
+    url: 'redis://localhost:6379',
+  });
+  redisClient.on('error', (e) => {
+    Logger.error('Could not establish a connection with redis. ' + e);
+  });
+  redisClient.on('connect', () => {
+    Logger.verbose('Connected to redis successfully', 'Bootstrap');
+  });
+  redisClient.connect().catch(console.error);
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient, logErrors: true }),
+      secret: 'secret-key',
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        secure: true,
+        sameSite: 'strict',
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60 * 24 * 3,
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
